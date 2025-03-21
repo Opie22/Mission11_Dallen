83411ae6008ec415f1e6206e8cd5b508617364a9
dallenopenshaw <dallen.openshaw@gmail.com>
2025-03-20 23:08:43 -0600

    working except for the pages data

--- Mission11_Openshaw_Backend/Mission11_Openshaw_Backend/Program.cs
+++ Mission11_Openshaw_Backend/Mission11_Openshaw_Backend/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<BookstoreContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
--- Mission11_Openshaw_Backend/Mission11_Openshaw_Backend/obj/Debug/net8.0/Mission11_Openshaw_Backend.AssemblyInfo.cs
+++ Mission11_Openshaw_Backend/Mission11_Openshaw_Backend/obj/Debug/net8.0/Mission11_Openshaw_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Openshaw_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26788da41b93e359ea4fbedfabf20edc7db2c43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05a4d2371d2e42ef7a3059f88f2c926e00232c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Openshaw_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Openshaw_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
